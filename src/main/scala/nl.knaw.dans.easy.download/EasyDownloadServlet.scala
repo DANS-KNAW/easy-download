6801913c2ae1cbc984243c1fa5db404539569bb4
alexanderatamas <alexander.atamas@dans.knaw.nl>
2017-11-13 16:18:33 +0100

    configured a bag store

--- src/main/scala/nl.knaw.dans.easy.download/EasyDownloadServlet.scala
+++ src/main/scala/nl.knaw.dans.easy.download/EasyDownloadServlet.scala
@@ -13,10 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package nl.knaw.dans.easy.download
 
 import nl.knaw.dans.lib.logging.DebugEnhancedLogging
 import org.scalatra._
+
+
+
+
+
 
 class EasyDownloadServlet(app: EasyDownloadApp) extends ScalatraServlet with DebugEnhancedLogging {
   import app._
