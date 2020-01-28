e145d8eef1153914ae0a915773acf9d858db483e
Richard van Heest <richard.van.heest@dans.knaw.nl>
2020-01-28 10:30:44 +0100

    check for existence and nonemptiness of statistics file before uploading

--- src/main/assembly/dist/bin/send-statistics.sh
+++ src/main/assembly/dist/bin/send-statistics.sh
@@ -5,6 +5,17 @@ YESTERDAY=`date --date=yesterday +%Y-%m-%d`
 LOG_FILE="$1/easy-statistics.$YESTERDAY.log"
 LOG_FILE_TARGET="$2/statistics.log.${YESTERDAY}_dls"
 
-echo "Sending yesterday's statistics logging ... "
-scp $LOG_FILE $LOG_FILE_TARGET
+if [ -f "$LOG_FILE" ]; then
+  LINE_COUNT=$(wc -l < "$LOG_FILE")
+  if [[ $LINE_COUNT -gt 0 ]]; then
+    echo "Sending yesterday's statistics logging..."
+    scp $LOG_FILE $LOG_FILE_TARGET
+    echo "Sent yesterday's statistics logging."
+  else
+    echo "File $LOG_FILE exists, but is empty; nothing to send"
+  fi
+else
+  echo "File $LOG_FILE does not exist; nothing to send"
+fi
+
 exit $?
